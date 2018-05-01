fc0ee50f0a9a3cf40bf3764843724aaf825eb129
kim gi chul <21500048@handong.edu>
2018-05-01 11:08:36 +0900

    prob01

--- src/main/java/edu/handong/csee/java/exceptionhandle/example/prob1/SpeedLimitter.java
+++ src/main/java/edu/handong/csee/java/exceptionhandle/example/prob1/SpeedLimitter.java
@@ -1,10 +1,27 @@
 package edu.handong.csee.java.exceptionhandle.example.prob1;
 
 public class SpeedLimitter {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private int speed = 0;
+	private int limit = 0;
+	public SpeedLimitter(int limit, int speed)
+	{
+		this.limit = limit;
+		this.speed = speed;
 	}
 
+	public void warnSpeedLimit()
+	{
+		try {
+			if(this.speed > this.limit)
+			{
+				throw new Exception("Speed Limit " + this.limit + "km exceeded!");
+			}
+			System.out.println("You are a law abiding citizen!");
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+			System.out.println("You are being fined. ");
+		}
+		System.out.println("Your current speed: " + this.speed);
+	}
 }
+
