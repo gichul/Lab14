b75d7d6112777143e766320ccd66128f3e088b46
kim gi chul <21500048@handong.edu>
2018-05-07 16:59:57 +0900

    prob1

--- src/main/java/edu/handong/csee/java/exceptionhandle/example/prob1/SpeedLim_main.java
+++ src/main/java/edu/handong/csee/java/exceptionhandle/example/prob1/SpeedLim_main.java
@@ -1,10 +1,22 @@
-package edu.handong.csee.java.exceptionhandle.example.prob1;
+package edu.handong.csee.java.exceptionhandle.example.prob1; //package name 
 
-public class SpeedLim_main {
+import java.util.Scanner;	//import Scanner to use Scanner class
+import java.util.Random;	//import Random to use Random class 
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class SpeedLim_main {	//declare main class
 
+	public static void main(String[] args) {		//declare main method
+	
+		System.out.printf("Set the speed limit, officer : ");	//print this ""line on console
+		
+		Scanner keyboard=new Scanner(System.in);	//instantiation to use keyboard
+		Random rand=new Random();			//instantiation to use Random number
+		int limit=keyboard.nextInt();		//declare limit and input data from keyboard 
+		int speed=rand.nextInt(100);		//declare speed and input data 0~100
+		
+		SpeedLimitter speed1=new SpeedLimitter(limit,speed);	//In constructor input the data
+		speed1.warnSpeedLimit();	//call warnSpeedLimit method
+		
+	
 	}
-
 }
--- src/main/java/edu/handong/csee/java/exceptionhandle/example/prob1/SpeedLimitter.java
+++ src/main/java/edu/handong/csee/java/exceptionhandle/example/prob1/SpeedLimitter.java
@@ -1,27 +1,33 @@
-package edu.handong.csee.java.exceptionhandle.example.prob1;
+package edu.handong.csee.java.exceptionhandle.example.prob1;//package name
 
-public class SpeedLimitter {
-	private int speed = 0;
-	private int limit = 0;
-	public SpeedLimitter(int limit, int speed)
-	{
-		this.limit = limit;
-		this.speed = speed;
+public class SpeedLimitter {		//declare SpeedLimiter class 
+	private int speed;			//declare instance variable speed
+	private int limit;			//declare instance variable limit
+	
+	public SpeedLimitter(int limit,int speed) {		//declare constructor
+		this.speed=speed;				//initiation of this.speed
+		this.limit=limit;				//initiation of this.limit
+		
 	}
-
-	public void warnSpeedLimit()
-	{
-		try {
-			if(this.speed > this.limit)
-			{
-				throw new Exception("Speed Limit " + this.limit + "km exceeded!");
+	
+	void warnSpeedLimit(){		//declare warnSpeedLimit method
+		try {		//declare try&catch statement
+			if(this.speed>this.limit) {		//compare to make exception
+				throw new Exception();  //if the upper statement true call the Exception() 
 			}
-			System.out.println("You are a law abiding citizen!");
-		}catch(Exception e) {
-			System.out.println(e.getMessage());
-			System.out.println("You are being fined. ");
+			else { //if the 'if'statement is false executes this statement
+				System.out.println("You are a law abiding citizen!"); // print this statement on console
+				
+			}
+			
+			
 		}
-		System.out.println("Your current speed: " + this.speed);
+		catch(Exception e) {		//When the Exception occurred call this catch statement.
+			System.out.println("speed limit is "+this.limit+"km !"); //print this line on console
+			System.out.println("you are being fined"); //print this line on console
+		}
+		System.out.println("Your current Speed : "+this.speed); 		//print the current Speed on console 
 	}
+	
 }
 
