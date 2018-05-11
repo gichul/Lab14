5adb14da2d8bce2046ff6ee8f6d70a59e2c3a681
gichulkim <gichulkim@172.17.140.64>
2018-05-11 15:12:10 +0900

    prob3 singleline comment

--- src/main/java/edu/handong/csee/java/exceptionhandle/example/prob3/Powercalc.java
+++ src/main/java/edu/handong/csee/java/exceptionhandle/example/prob3/Powercalc.java
@@ -1,36 +1,36 @@
-package edu.handong.csee.java.exceptionhandle.example.prob3;
+package edu.handong.csee.java.exceptionhandle.example.prob3;	//package name
 
 
-public class Powercalc {
+public class Powercalc {		//Powercalc class
 
 	
-	public Powercalc() {
+	public Powercalc() {		//constructor
 	
 	}
 	
-	public long power(int n,int p) throws Exception {
-		long result = 0;
+	public long power(int n,int p) throws Exception { 	//power method throws Exception
+		long result = 0;		//long type variable result
 		
-		result=(long)Math(n,p);
-		if(n<0||p<0) {
-			throw new Exception("n or p should not be negative");
+		result=(long)Math(n,p);		//input the data n,p and calculate square 
+		if(n<0||p<0) {			//if n&p<0 
+			throw new Exception("n or p should not be negative"); //make the instance and using throw represent Exception
 		}
-		if(n==0&&p==0) {
-			throw new Exception("n and p should not be zero");
+		if(n==0&&p==0) {  //if n==0&&p==0
+			throw new Exception("n and p should not be zero"); //make the instance and using throw represent Exception
 		}
-		return result;
+		return result; //return the square value of result
 	}
 
 	
 	
 	
-	private long Math(int n, int p) {
+	private long Math(int n, int p) {  //Math method
 		// TODO Auto-generated method stub
-		long square=1;
-		for(int i=0; i<p;i++) {
-		square*=n;
+		long square=1;			//long type variable square
+		for(int i=0; i<p;i++) {  //for statement to * as p numbers
+		square*=n;		//calculates square value
 		}
-		return square;
+		return square;		//return data to result  
 	}
 	
 	
--- src/main/java/edu/handong/csee/java/exceptionhandle/example/prob3/PowercalcMain.java
+++ src/main/java/edu/handong/csee/java/exceptionhandle/example/prob3/PowercalcMain.java
@@ -1,25 +1,25 @@
-package edu.handong.csee.java.exceptionhandle.example.prob3;
+package edu.handong.csee.java.exceptionhandle.example.prob3;  //package name
 
-import java.util.Scanner;
+import java.util.Scanner;  //import Scanner class to use keyboard
 
-public class PowercalcMain {
+public class PowercalcMain { //declare PowercalcMain class
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { //main method
 		// TODO Auto-generated method stub
 
-		Powercalc my_calculator=new Powercalc();
-		int n,p;
+		Powercalc my_calculator=new Powercalc();	//instantiation of Powercalc class
+		int n,p;		//instance variable
 
-		Scanner keyboard=new Scanner(System.in);
-		while(keyboard.hasNextInt()) {
+		Scanner keyboard=new Scanner(System.in); //instantiation of Scanner class
+		while(keyboard.hasNextInt()) {		//while statement using hasNextInt()instance
 
-			n=keyboard.nextInt();
-			p=keyboard.nextInt();
+			n=keyboard.nextInt(); //input int type data into n
+			p=keyboard.nextInt(); //input int type data into p
 
-			try {
-				System.out.println(my_calculator.power(n, p));
-			}catch(Exception e) {
-				System.out.println(e);
+			try {						//try statement to use Exception
+				System.out.println(my_calculator.power(n, p)); //call the method power and print on console
+			}catch(Exception e) {		//catch statement receiving the instance
+				System.out.println(e);		//
 			}
 		}
 
