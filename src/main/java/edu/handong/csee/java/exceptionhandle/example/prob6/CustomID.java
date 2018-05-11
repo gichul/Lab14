fcd2e1bc366b7416142c9b0eb12c22a62d62d376
gichulkim <gichulkim@172.17.140.64>
2018-05-11 21:30:51 +0900

    prob6

--- src/main/java/edu/handong/csee/java/exceptionhandle/example/prob6/CustomID.java
+++ src/main/java/edu/handong/csee/java/exceptionhandle/example/prob6/CustomID.java
@@ -1,59 +1,59 @@
-package edu.handong.csee.java.exceptionhandle.example.prob6;
+package edu.handong.csee.java.exceptionhandle.example.prob6; //package name
 
-public class CustomID extends Exception{
-	private String[] races = new String[]{"Vulcans", "Romulan", "Klingons"};
-	private String name = "";
-	private int age = 0;
-	private String race = "";
-	private int state = 0;
+public class CustomID extends Exception{ //class CustomID extends Exception
+	private String[] races = new String[]{"Vulcans", "Romulan", "Klingons"}; //declare the three races to distinguish
+	private String name = ""; 	//declare String name
+	private int age = 0;		//declare int age
+	private String race = "";		//declare String race
+	private int state = 0;		//declare int state
 
-	public void set_name(String str) throws Exception
+	public void set_name(String str) throws Exception  //set_name  throws Exception
 	{
-		if(str.length() < 5) {
-			state = 0;
-			throw new Exception("Your name is short! Try again!");
+		if(str.length() < 5) { //if statement to check the length of str
+			state = 0;		//use state variable
+			throw new Exception("Your name is short! Try again!");  //print the result on console
 		}
-
-		else {
-			this.name = str;
-			System.out.println("Name is valid");
-			System.out.println("Name: " + this.name);
-			state++;
+		
+		else {		//else statement 
+			this.name =str;	 //input the str into name instance variable
+			System.out.println("Name is valid"); //print this on console
+			System.out.println("Name: " + name);		//print this on console
+			state++; //if the if statement is true return 0, if not return 1
 		}
 	}
 
 
 
-	public void set_age(int num) throws Exception
+	public void set_age(int num) throws Exception//set_age  throws Exception
 	{
-		if(num < 18) {
-			state = 1;
-			throw new Exception("You are too young! Try again!");
+		if(num < 18) {//if statement to check the length of num
+			state = 1;	//if the statement is true statement is 1
+			throw new Exception("You are too young! Try again!");  //making instance and throw the String
 		}
-		else {
-			this.age = num;
-			System.out.println("Age is valid");
-			System.out.println("Age: " + this.age);
-			state++;
+		else {		//else statement
+			this.age = num;		//input the num into age instance variable
+			System.out.println("Age is valid");	//print on console
+			System.out.println("Age: " + this.age);	//print on console
+			state++;	//if the if statement is true return 0, if not return 1
 		}
 	}
 
-	public void set_race(String race) throws Exception{
-		for(int i = 0; i < races.length; i++)
+	public void set_race(String race) throws Exception{//set_race  throws Exception
+		for(int i = 0; i < races.length; i++) //for statement to check equality of races alphabet
 		{
-			if(races[i].equals(race)) {
-				this.race = races[i];
-				System.out.println("Race is valid");
-				System.out.println("Race: " + races[i]);
-				state = 0;
-				return;
+			if(races[i].equals(race)) {  //if statement comparing
+				this.race = races[i];  //input the data from races into race
+				System.out.println("Race is valid"); //print the line on console
+				System.out.println("Race: " + races[i]);//print the races[i]on console
+				state = 0;  //if the statement is true return 0
+				return; //return the value
 			}
 		}
-		state = 2;
-		throw new Exception("Human! Try again.");
+		state = 2;	//if the if statement is true return 2
+		throw new Exception("Human! Try again."); //making instance and throw the String
 	}
-	public int getState()
+	public int getState() //getState method to return value
 	{
-		return state;
+		return state; // return value
 	}
 }
--- src/main/java/edu/handong/csee/java/exceptionhandle/example/prob6/CustomIDMain.java
+++ src/main/java/edu/handong/csee/java/exceptionhandle/example/prob6/CustomIDMain.java
@@ -1,41 +1,42 @@
-package edu.handong.csee.java.exceptionhandle.example.prob6;
-import java.util.Scanner;
+package edu.handong.csee.java.exceptionhandle.example.prob6; //package name
+import java.util.Scanner; //import Scanner class 
 
-public class CustomIDMain {
+public class CustomIDMain {		//CustomIDMain class
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {	//static main method
 		// TODO Auto-generated method stub
-		String name, race;
-		int age = 0;
-		Scanner keyboard = new Scanner(System.in);
-		CustomID obj = new CustomID();
-		boolean repeat = true;
+		String name, race;		//declare String name & race
+		int age = 0;		//declare instance variable
+		Scanner keyboard = new Scanner(System.in);		//instantiation to use keyboard
+		CustomID obj = new CustomID();		//instantiation of CustomID
+		boolean repeat = true;		//declare boolean type instance variable
 
-		while(repeat)
+		while(repeat)		//while statement
 		{
-			try {
-				if(obj.getState() == 0) {
-					System.out.print("Enter your name: ");
-					name = keyboard.nextLine();
-					obj.set_name(name);
+			try {		//try statement
+				if(obj.getState() == 0) {	//compare the value of CustomID`s instance variable state
+					System.out.print("Enter your name: ");//print this line on console
+					name = keyboard.nextLine();	//input the data from keyboard
+					obj.set_name(name);		//input the name String into set_name method
 				}
-				else if(obj.getState() == 1) {
-					System.out.print("Enter your age: ");
-					age = keyboard.nextInt();
-					keyboard.nextLine();
-					obj.set_age(age);
+				else if(obj.getState() == 1) {	//compare the value of CustomID`s instance variable state
+					System.out.print("Enter your age: ");////print on console
+					age = keyboard.nextInt(); //input data from keyboard
+					
+					obj.set_age(age);	//input the age into age method
 				}
 
 
-				else if(obj.getState() == 2) {
-					System.out.print("Enter your race: ");
-					race = keyboard.nextLine();
-					obj.set_race(race);
+				else if(obj.getState() == 2) {	//compare the value of CustomID`s instance variable state
+					System.out.print("Enter your race: "); //print on console
+					race = keyboard.nextLine(); //input data from keyboard
+					keyboard.nextLine(); //to remove NULL character 
+					obj.set_race(race);  //input the race String into race method
 				}
 			}
-			catch(Exception e){
-				System.out.println(e.getMessage());
-				continue;
+			catch(Exception e){ //catch statement checking Exception 
+				System.out.println(e.getMessage()); //print the error receiving from CustomID String
+				continue; //back to beginning
 			}
 		}
 
